b235df41b86bc3b985d5d789ac6b92b7467ffc76
fanwei <546271337@qq.com>
2019-09-29 15:12:41 +0800

    取消自动填充密码

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://118.190.53.31:8080"'
+  BASE_API: '"http://localhost:8080"'
 })
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -78,8 +78,8 @@
       };
       return {
         loginForm: {
-          username: 'admin',
-          password: '123456',
+          username: '',
+          password: '',
         },
         loginRules: {
           username: [{required: true, trigger: 'blur', validator: validateUsername}],
